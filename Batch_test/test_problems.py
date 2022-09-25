b35afa3feabb84de9f702f139bc922ecc67f4182
Zhang Cheng <hustquick@163.com>
2022-09-25 10:05:13 +0800

    修正了一些错误。

--- Batch_test/test_problems.py
+++ Batch_test/test_problems.py
@@ -5,9 +5,8 @@ files = os.listdir('../' + folder)
 files.sort()
 os.chdir(f'../{folder}')
 for file in files:
-    if file.endswith(".pdf"):
-        continue
-    print('*'*10 + f' {file} ' + '*'*10)
-    os.system(r'python ' + file)
-os.chdir('../test')
+    if file.endswith(".py"):
+        print('*'*10 + f' {file} ' + '*'*10)
+        os.system(r'python ' + file)
+os.chdir('../Batch_test')
 print('-'*20 + '运行完毕' + '-'*20)
